9bbf6fd7d43b99b83436f7d828bbca3ffa7d95e1
Jack Rees Kelly <32745647+jackkelly75@users.noreply.github.com>
2019-05-13 12:08:17 +0100

    Add files via upload

---
+++ global.R
@@ -0,0 +1,14 @@
+
+require(shiny)
+require(DT)
+
+
+load("data/brown4.Rdata")
+load("data/coral1.Rdata")
+
+load("data/darkseagreen4.Rdata")
+load("data/navajowhite2.Rdata")
+load("data/orangered4.Rdata")
+load("data/salmon.Rdata")
+load("data/thistle2.Rdata")
+load("data/violet.Rdata")
