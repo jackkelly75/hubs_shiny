8fd2c12c9bc3ef9a0e2f58c647127663a3780208
Jack Rees Kelly <32745647+jackkelly75@users.noreply.github.com>
2019-09-29 23:21:28 +0100

    Update ui.R

--- AD/ADHC/ui.R
+++ AD/ADHC/ui.R
@@ -5,8 +5,6 @@ fluidPage(
 	downloadButton("downloadRedData", "red.csv"),
 	downloadButton("downloadSkyblueData", "skyblue.csv"),
 	downloadButton("downloadOrangeData", "darkorange2.csv"),
-	downloadButton("downloadLightData", "lightsteelblue1.csv"),
-	downloadButton("downloadMidnightData", "midnightblue.csv"),
 
     mainPanel(
     	tabsetPanel(
@@ -14,9 +12,7 @@ fluidPage(
 	        tabPanel("darkolivegreen", DT::dataTableOutput("mytable1")),
 	        tabPanel("red", DT::dataTableOutput("mytable2")),
 	        tabPanel("skyblue", DT::dataTableOutput("mytable3")),
-	        tabPanel("darkorange2", DT::dataTableOutput("mytable4")),
-	        tabPanel("lightsteelblue1", DT::dataTableOutput("mytable5")),
-	        tabPanel("midnightblue", DT::dataTableOutput("mytable6"))
+	        tabPanel("darkorange2", DT::dataTableOutput("mytable4"))
 		)
    	)
 )
