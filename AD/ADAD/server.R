c631e6a4a0df9e6a2f6685e71e7ddde57d6d138f
Jack Rees Kelly <32745647+jackkelly75@users.noreply.github.com>
2019-09-29 23:18:49 +0100

    Update server.R
    Remove pale turqiouse

--- AD/ADAD/server.R
+++ AD/ADAD/server.R
@@ -7,10 +7,6 @@ function(input, output) {
   output$mytable1 <- DT::renderDataTable(
      DT::datatable(blue, options = list(pageLength = 25), filter ="top")
     )
-   
-  output$mytable2 <- DT::renderDataTable(
-     DT::datatable(paleturquoise, options = list(pageLength = 25), filter ="top")
-    )
 
   output$downloadBlueData <- downloadHandler(
   	filename = "blue.csv",
@@ -19,11 +15,5 @@ function(input, output) {
   	}
   )
 
-  output$downloadPaleData <- downloadHandler(
-  	filename = "paleturquoise.csv",
-  	content = function(file){
-  	  	write.csv(paleturquoise, file , row.names = FALSE)
-  	}
-  )
 
 }
