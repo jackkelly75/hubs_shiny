5ce0be5dcc70136f69109fb5abf25dea8aa0210a
Jack Rees Kelly <32745647+jackkelly75@users.noreply.github.com>
2019-09-29 23:19:21 +0100

    Update ui.R
    remove paleturqoiuse

--- AD/ADAD/ui.R
+++ AD/ADAD/ui.R
@@ -2,13 +2,11 @@ fluidPage(
 	titlePanel("AD modules from AD dataset not preserved in one or both of MCI and HC"),
 
 	downloadButton("downloadBlueData", "blue.csv"),
-	downloadButton("downloadPaleData", "paleturquoise.csv"),
 
     mainPanel(
     	tabsetPanel(
 	        id = 'dataset',
-	        tabPanel("blue", DT::dataTableOutput("mytable1")),
-	        tabPanel("paleturquoise", DT::dataTableOutput("mytable2"))
+	        tabPanel("blue", DT::dataTableOutput("mytable1"))
 		)
    	)
 )
