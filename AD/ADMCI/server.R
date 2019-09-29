1e869719f52fcb0684020a5f403bff17ee73a5ea
Jack Rees Kelly <32745647+jackkelly75@users.noreply.github.com>
2019-09-29 23:22:47 +0100

    Update server.R

--- AD/ADMCI/server.R
+++ AD/ADMCI/server.R
@@ -4,32 +4,12 @@ library(DT)
 
 function(input, output) {
   
-  output$mytable1 <- DT::renderDataTable(
-     DT::datatable(darkgrey, options = list(pageLength = 25), filter ="top")
-    )
-   
-  output$mytable2 <- DT::renderDataTable(
-     DT::datatable(saddlebrown, options = list(pageLength = 25), filter ="top")
-    )
  
   output$mytable3 <- DT::renderDataTable(
      DT::datatable(sienna3, options = list(pageLength = 25), filter ="top")
     )
 
-  output$downloadDarkgreyData <- downloadHandler(
-  	filename = "darkgrey.csv",
-  	content = function(file){
-  	  	write.csv(darkgrey, file , row.names = FALSE)
-  	}
-  )
-
-
-  output$downloadSaddlebrownData <- downloadHandler(
-  	filename = "saddlebrown.csv",
-  	content = function(file){
-  	  	write.csv(saddlebrown, file , row.names = FALSE)
-  	}
-  )
+ 
 
   output$downloadSienna3Data <- downloadHandler(
   	filename = "sienna3.csv",
