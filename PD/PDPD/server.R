e40aed8d9eb22493187f741234ff0394d3077c62
Jack Rees Kelly <32745647+jackkelly75@users.noreply.github.com>
2019-09-29 23:26:19 +0100

    Update server.R

--- PD/PDPD/server.R
+++ PD/PDPD/server.R
@@ -4,13 +4,7 @@ library(DT)
 
 function(input, output) {
   
-  output$mytable1 <- DT::renderDataTable(
-     DT::datatable(brown4, options = list(pageLength = 25), filter ="top")
-    )
-   
-  output$mytable2 <- DT::renderDataTable(
-     DT::datatable(coral1, options = list(pageLength = 25), filter ="top")
-    )
+
 
   output$mytable3 <- DT::renderDataTable(
      DT::datatable(darkseagreen4, options = list(pageLength = 25), filter ="top")
@@ -25,31 +19,7 @@ function(input, output) {
      DT::datatable(salmon, options = list(pageLength = 25), filter ="top")
     )
 
-  output$mytable6 <- DT::renderDataTable(
-     DT::datatable(thistle2, options = list(pageLength = 25), filter ="top")
-    )
 
-  output$mytable7 <- DT::renderDataTable(
-     DT::datatable(violet, options = list(pageLength = 25), filter ="top")
-    )
-
-  output$mytable8 <- DT::renderDataTable(
-     DT::datatable(orangered4, options = list(pageLength = 25), filter ="top")
-    )
-
-
-  output$downloadBrown4Data <- downloadHandler(
-    filename = "brown4.csv",
-    content = function(file){
-        write.csv(brown4, file , row.names = FALSE)
-    }
-  )
-  output$downloadCoral1Data <- downloadHandler(
-    filename = "coral1.csv",
-    content = function(file){
-        write.csv(coral1, file , row.names = FALSE)
-    }
-  )
   output$downloadDarkData <- downloadHandler(
     filename = "darkseagreen4.csv",
     content = function(file){
@@ -68,24 +38,6 @@ function(input, output) {
         write.csv(salmon, file , row.names = FALSE)
     }
   )
-  output$downloadThistle2Data <- downloadHandler(
-    filename = "thistle2.csv",
-    content = function(file){
-        write.csv(thistle2, file , row.names = FALSE)
-    }
-  )
-  output$downloadVioletData <- downloadHandler(
-    filename = "violet.csv",
-    content = function(file){
-        write.csv(violet, file , row.names = FALSE)
-    }
-  )
 
-  output$downloadOrangered4Data <- downloadHandler(
-    filename = "orangered4.csv",
-    content = function(file){
-        write.csv(orangered4, file , row.names = FALSE)
-    }
-  )
 
 }
