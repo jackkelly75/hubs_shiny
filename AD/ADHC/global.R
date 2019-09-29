5b0df82f298dc8312565f02d5a1d7ff8142c8770
Jack Rees Kelly <32745647+jackkelly75@users.noreply.github.com>
2019-09-29 23:20:31 +0100

    Update global.R

--- AD/ADHC/global.R
+++ AD/ADHC/global.R
@@ -6,7 +6,4 @@ require(DT)
 load("data/darkolivegreen.Rdata")
 load("data/red.Rdata")
 load("data/skyblue.Rdata")
-
 load("data/darkorange2.Rdata")
-load("data/lightsteelblue1.Rdata")
-load("data/midnightblue.Rdata")
