f79f1c0af671f6bd1ca4f42422d3699bb723afaa
Jack Rees Kelly <32745647+jackkelly75@users.noreply.github.com>
2019-09-29 23:24:40 +0100

    Update ui.R

--- PD/PDHC/ui.R
+++ PD/PDHC/ui.R
@@ -2,14 +2,12 @@ fluidPage(
 	titlePanel("HC modules not preserved in PD"),
 
 	downloadButton("downloadPurpleData", "purple.csv"),
-	downloadButton("downloadBlueData", "blue.csv"),
 
 
     mainPanel(
     	tabsetPanel(
 	        id = 'dataset',
-	        tabPanel("purple", DT::dataTableOutput("mytable1")),
-	        tabPanel("blue", DT::dataTableOutput("mytable2"))
+	        tabPanel("purple", DT::dataTableOutput("mytable1"))
 		)
    	)
 )
