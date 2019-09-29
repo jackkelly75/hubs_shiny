9a84c5d7e03bb1de9a228909690813ebb22a49ef
Jack Rees Kelly <32745647+jackkelly75@users.noreply.github.com>
2019-09-29 23:23:10 +0100

    Update ui.R

--- AD/ADMCI/ui.R
+++ AD/ADMCI/ui.R
@@ -1,8 +1,6 @@
 fluidPage(
 	titlePanel("MCI modules from AD dataset not preserved in one or both of AD and HC"),
 
-	downloadButton("downloadDarkgreyData", "darkgrey.csv"),
-	downloadButton("downloadSaddlebrownData", "saddlebrown.csv"),
 	downloadButton("downloadSienna3Data", "sienna3.csv"),
 
 
@@ -10,8 +8,6 @@ fluidPage(
     mainPanel(
     	tabsetPanel(
 	        id = 'dataset',
-	        tabPanel("darkgrey", DT::dataTableOutput("mytable1")),
-	        tabPanel("saddlebrown", DT::dataTableOutput("mytable2")),
 	        tabPanel("sienna3", DT::dataTableOutput("mytable3"))
 		)
    	)
